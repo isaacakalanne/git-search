0943fa239391c684a2f17160a2cf579a350fe558
Isaac <isaacakalanne@gmail.com>
2020-05-02 13:04:56 +0100

    Created Repository.swift file, containing structs of JSON response for future rest api query.

---
+++ GitSearch/Repository.swift
@@ -0,0 +1,29 @@
+//
+//  Repository.swift
+//  GitSearch
+//
+//  Created by Isaac Akalanne on 02/05/2020.
+//  Copyright © 2020 Believe And Succeed Apps. All rights reserved.
+//
+
+import Foundation
+
+struct Repositories:Decodable {
+    var items:[RepositoryDetail]
+}
+
+struct RepositoryDetail:Decodable {
+    var name:String
+    var owner:OwnerDetail
+    var description:String
+    
+    var language:String
+    var forks_count:Int
+    var open_issues_count:Int
+}
+
+struct OwnerDetail:Decodable {
+    var login:String
+    var id:Int
+    var avatar_url:String
+}
